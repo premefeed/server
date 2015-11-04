1922489e01b5c1c8a114c588c3a7f3074a70a687
Peter Soboyejo <thepcmrtim@gmail.com>
2015-11-03 19:47:13 -0500

    I so didn't get any work done today

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
+// server.js
+// Peter Soboyejo
+// http://www.github.com/dzt
+
+
 var request = require('request'),
     cheerio = require('cheerio'),
     express = require('express'),
     fs = require('fs'),
     open = require('open'),
+    Crawler = require('simplecrawler'),
     app = express();
 
+var url = "http://www.supremenewyork.com/shop/all";
+
+var crawler = Crawler.crawl(url);
+crawler.interval = 10000;
+crawler.maxConcurrency = 1;
+
+var mins = 0.05,
+    interval_a = mins * 60 * 1000
+
+
+crawler.on("fetchcomplete", function (queueItem) {
 
 // Make a GET Request under '/collect' that retunrs JSON data of
-app.get('/api/v1/scrape', function(req, res) {
-  request('http://www.supremenewyork.com/shop/all', function(err, resp, html, rrr, body) {
+  request(url, function(err, resp, html, rrr, body) {
 
         /*
+
         When making a request to the site if it retunrs back a response of 200 then
+
         */
 
         String.prototype.capitalizeEachWord = function()
@@ -36,6 +54,28 @@ app.get('/api/v1/scrape', function(req, res) {
             var availability = nextElement.text().capitalizeEachWord();
             var itemLink = "http://www.supremenewyork.com" + $('#container').find('a').attr('href');
 
+            console.log("Latest Product: ", title);
+
+            /*
+            fs.readFile('output.json', function(err, data) {
+
+              if (err) throw err;
+              var obj = JSON.parse(data);
+              if (obj != parsedResults) {
+
+
+                  console.log("Something has changed");
+
+                  // TODO: twilio, nodemailer, gcm 
+
+              }
+            });
+            */
+
+            fs.writeFile('output.json', JSON.stringify(parsedResults, null, 4), function (error) {
+                console.log('Output file successfully written');
+            });
+
             if (availability == "") availability = "Available";
 
             var metadata = {
@@ -44,13 +84,18 @@ app.get('/api/v1/scrape', function(req, res) {
               imageLink: imageLink,
               availability: availability
             };
+
             parsedResults.push(JSON.stringify(metadata));
+
           });
-          console.log(JSON.stringify(parsedResults));
-          res.send(JSON.stringify(parsedResults));
+          
+          // res.json(JSON.stringify(parsedResults));
+
         }
   });
 });
+
+
 
 app.get('/', function(req, res) {
 
@@ -64,7 +109,13 @@ app.get('/api/v1/', function(req, res) {
     
 });
 
+app.get('/api/v1/scrape', function(req, res) {
+
+    res.sendFile(__dirname + '/output.json');
+    
+});
+
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  open('http://localhost:'+this.address().port+'/');
+  // open('http://localhost:'+this.address().port+'/');
 });
