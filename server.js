0dd11ac092b05f08b041050f582475b2cbb77dfd
Peter Soboyejo <thepcmrtim@gmail.com>
2015-12-24 07:53:57 -0500

    No need for the test crap anymore

--- server.js
+++ server.js
@@ -163,14 +163,6 @@ app.get('/', function(req, res) {
     res.render('index');
 });
 
-app.get('/test', function(req, res) {
-
-    fs.readFile('test.json', function(err, data) {
-        res.send(JSON.parse(data));
-    });
-
-});
-
 app.get('/api/v1/recent', function(req, res) {
 
 
